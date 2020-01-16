b8a67bdf6bccf71501e29cc36d79128d3633a5a9
totanakko <58853742+totanakko@users.noreply.github.com>
2020-01-16 21:01:13 +0300

    Home Work 1, Boxing, generator password, and fool).py

--- HomeWorks/Вербовецкий Иван/Boxing Legend.3.py
+++ HomeWorks/Вербовецкий Иван/Boxing Legend.3.py
@@ -17,3 +17,19 @@ XXL = 10150
 XXXL = ((XXL / L) * 100)
 print(XXXL)
 
+#Number 3 
+# -*- coding:utf -8 -*-
+#!/usr/bin/python3
+
+import random
+
+chars = '1234567890QWERTYUIOPASDFGHJKKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'
+number = input('количество паролей?'+ "\n")
+length = input('длина пароля?'+ "\n")
+number = int(number)
+length = int(length)
+for n in range(number):
+    pasword =''
+    for i in range(length):
+        pasword += random.choice(chars)
+        print(pasword)

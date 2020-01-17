b0b04488092b7e69b558bb81bc70c542b19a9e58
totanakko <58853742+totanakko@users.noreply.github.com>
2020-01-17 15:04:03 +0300

    HomeWork.2

---
+++ HomeWorks/Вербовецкий Иван/HomeWork.2.py
@@ -0,0 +1,28 @@
+#Number1
+#List#coding
+List = ["cat", "the mouse", "dog", "a monkey", "Giraffe", "Elephant"]
+del List[3]
+print(List)
+del List[4]
+print(List)
+XD
+#Number2
+
+a = "Аббревиатура"
+
+print(a)
+
+a = list(a)
+
+print(a)
+
+del a[0]
+del a[6]
+del a[9]
+
+print(a)
+
+a = "".join(a)
+
+print(a)
+#Number3
--- HomeWorks/Вербовецкий Иван/Homework.1.py
+++
@@ -1,6 +0,0 @@
-List = ["cat", "the mouse", "dog", "a monkey", "Giraffe", "Elephant"]
-del List[3]
-print(List)
-del List[4]
-print(List)
-XD
